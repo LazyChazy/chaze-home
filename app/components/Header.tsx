9e5000eb405bb32b7f3d1ce314a05ba611fcd3f6
LazyChazy <74274533+LazyChazy@users.noreply.github.com>
2024-09-13 00:07:40 -0600

    Testing with React

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// components/Header.tsx
+export default function Header() {
+    return (
+      <header>
+        <h1>Chaze's Portfolio</h1>
+        {/* Navigation items */}
+      </header>
+    )
+  }
